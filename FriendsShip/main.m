46f09622a004419d05fa23b6e03f589d4d4c050b
Halid Cisse <halidoucse@gmail.com>
2016-05-02 13:05:38 +0100

    Initial Commit

---
+++ FriendsShip/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FriendsShip
+//
+//  Created by Halid Cisse on 5/2/16.
+//  Copyright © 2016 Halid Cisse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
